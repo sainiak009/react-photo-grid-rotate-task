8f65a3471cd5b291fb407aed864dea79ea4db535
Saini <212771195@logon.ds.ge.com>
2020-05-03 21:16:24 +0530

    Working code v1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ImagesData from './imagesData.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+                  angle: 0
+                  }
+  }
+
+  rotateImage() {
+    let angleEle = document.getElementById('angle')
+    this.setState({angle:angleEle.value})
+  }
+
+  render() {
+    console.log(ImagesData)
+
+    let n = ImagesData.length+1
+    let angle = this.state.angle
+    let gridData = ImagesData
+    let stepMax = 50*n
+
+    var rows = gridData.map(function (imgRow, i){
+            var partImg = imgRow.map(function (imgSrc, j) {
+                return (
+                    <div className="column">
+                      <img src={imgSrc.addr} key={i*n+j} style={{transformOrigin: (stepMax-100*j)+'% '+(stepMax-100*i)+'%', transform: 'rotate('+angle+'deg)'}} />
+                    </div>
+                    );
+            });
+            return (
+                <div className="row" key={i}> {partImg} </div>
+             );
+      });
+
+    return (
+      <div className="App">
+          <div className="inputTag">
+              Enter the angle: <input type="number" name="angle" id="angle"/>
+              <button className="button" onClick={this.rotateImage.bind(this)}>Rotate</button>
+          </div>
+          <div className="container">
+              {rows}
+          </div>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
---
+++ src/imagesData.js
@@ -0,0 +1,37 @@
+const images = [
+[
+  {
+    addr: 'https://i.postimg.cc/crtmZTTr/image-part-001.jpg',
+  },
+  {
+    addr: 'https://i.postimg.cc/CBKG24zM/image-part-002.jpg',
+  },
+  {
+    addr: 'https://i.postimg.cc/nspY4xxD/image-part-003.jpg',
+  }
+],
+[
+  {
+    addr: 'https://i.postimg.cc/5YkBWFCB/image-part-004.jpg',
+  },
+  {
+    addr: 'https://i.postimg.cc/v4dWyycN/image-part-005.jpg',
+  },
+  {
+    addr: 'https://i.postimg.cc/YjhNf85b/image-part-006.jpg',
+  }
+],
+[
+  {
+    addr: 'https://i.postimg.cc/Mnb1KLBK/image-part-007.jpg',
+  },
+  {
+    addr: 'https://i.postimg.cc/9r9Gs0F9/image-part-008.jpg',
+  },
+  {
+    addr: 'https://i.postimg.cc/jCmH8PKK/image-part-009.jpg',
+  }
+]
+];
+
+export default images;
